78e42634ca57d37467b8b544e2d9befafb048b7c
unknown <francociddiaz@gmail.com>
2023-01-20 15:47:50 -0300

    sexto commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div id='about' className='text-black mt-20 text-center'>
         <h2 className='text-3xl font-semibold animacion-about text-center'> Sobre mi</h2>
         <div className='flex justify-center'>
-        <p className='mt-12 animacion-descripcion font-medium text-justify'>Hola! Mi nombre es Franco Cid, Front end Developer Apasionado por la programacion y el desarrollo web. <br />
+        <p className='mt-12 animacion-descripcion font-medium text-center'>Hola! Mi nombre es Franco Cid, Front end Developer Apasionado por la programacion y el desarrollo web. <br />
         Me encanta beber cafe y la Formula 1.        
         </p>
         </div>
