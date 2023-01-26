757828041cf1515d0b13c9f546b8a1d440cc8740
FrancoCidDiaz <108705732+FrancoCidDiaz@users.noreply.github.com>
2023-01-26 16:58:11 -0300

    Add files via upload

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -13,25 +13,26 @@ import logoGithub from '../img/logo-github.png'
 import blog from '../img/blog.png'
 import quiz from '../img/quiz-f1.png'
 import logoBootstrap from '../img/logo-bootstrap.png'
+import workoutTracker from '../img/workout-tracker.png'
 
 const Proyectos = () => {
   return (
     <div id='proyectos' className='pt-12 m-auto'>
     <h2 className='text-3xl font-semibold text-center mt-20 h-28'>Proyectos</h2>
-    <div className=' flex justify-center gap-32'>
+    <div className='grid grid-cols-2 grid-rows-2 place-content-center justify-center items-center mx-8 grid-proyectos'>
       <div>
-        <div className='w-100'>
+        <div className='w-100 '>
         <h3 className='font-semibold text-center'>Academia virtual
           
         </h3>
-        <a target="_blank" href="https://integrador-nucba-dise-o-web.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6' src={academia} alt="" /></a>
+        <a target="_blank" href="https://integrador-nucba-dise-o-web.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={academia} alt="" /></a>
         </div>
-        <div className='w-12 flex mx-28 gap-4 pt-8'>
+        <div className='w-12 flex justify-center mx-auto gap-4 pt-8'>
           <img  src={logoHtml} alt="" />
           <img  src={logoCss} alt="" />
         </div>
         <div>
-          <a target="_blank" href="https://github.com/FrancoCidDiaz/Integrador-nucba-dise-o-web"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000' src={logoGithub} alt="" /></a>
+          <a target="_blank" href="https://github.com/FrancoCidDiaz/Integrador-nucba-dise-o-web"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000 m-auto' src={logoGithub} alt="" /></a>
         </div>
         </div>
       
@@ -41,9 +42,9 @@ const Proyectos = () => {
           
         </h3>
         <a target="_blank" href="https://frontend-blog-9a1a.vercel.app/registro">
-        <img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6' src={blog} alt="" /></a>
+        <img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={blog} alt="" /></a>
         </div>
-        <div className='w-10 h-auto flex flex-row gap-2 pt-8'>
+        <div className='w-10 h-auto flex justify-center mx-auto gap-2 pt-8'>
           <img  src={logoReact} alt="" />
           <img  src={logoReactHook} alt="" />
           <img  src={logoRouter} alt="" />
@@ -52,18 +53,18 @@ const Proyectos = () => {
           <img  src={logoTailwind} alt="" />
         </div>
         <div>
-          <a target="_blank" href="https://github.com/FrancoCidDiaz/Frontend-Blog"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000' src={logoGithub} alt="" /></a>
+          <a target="_blank" href="https://github.com/FrancoCidDiaz/Frontend-Blog"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000 m-auto' src={logoGithub} alt="" /></a>
         </div>
         </div>
 
         <div>
-        <div className='w-100'>
+        <div className='w-100 mt-10 div-proyectos'>
         <h3 className='font-semibold text-center'>Quiz Formula 1
           
         </h3>
-        <a target="_blank" href="https://quiz-f1.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6' src={quiz} alt="" /></a>
+        <a target="_blank" href="https://quiz-f1.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={quiz} alt="" /></a>
         </div>
-        <div className='w-10  flex mx-24 gap-4 pt-8'>
+        <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
           <img  src={logoReact} alt="" />
           <img  src={logoBootstrap} alt="" />
           <img  src={logoRouter} alt="" />
@@ -72,6 +73,24 @@ const Proyectos = () => {
           <a target="_blank" href="https://github.com/FrancoCidDiaz/quiz-f1"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000' src={logoGithub} alt="" /></a>
         </div>
         </div>
+
+        <div>
+        <div className='w-100 mt-10 div-proyectos'>
+        <h3 className='font-semibold text-center'>Workout-Tracker
+          
+        </h3>
+        <a target="_blank" href="https://workout-counter.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={workoutTracker} alt="" /></a>
+        </div>
+        <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
+          <img  src={logoReact} alt="" />
+          <img  src={logoBootstrap} alt="" />
+          
+        </div>
+        <div>
+          <a target="_blank" href="https://github.com/FrancoCidDiaz/quiz-f1"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000' src={logoGithub} alt="" /></a>
+        </div>
+        </div>
+
 
 
      
