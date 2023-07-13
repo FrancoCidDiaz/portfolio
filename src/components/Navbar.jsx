dc661c130e2c44b75c8765f44d4965aaaf31c5cd
FrancoCidDiaz <franco.giovanni.cid.diaz@gmail.com>
2023-07-13 18:02:25 -0400

    1233

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-scroll'
 const Navbar = () => {
   return (
     <div className='h-16 bg-zinc-900 text-white fixed w-full'>
-        <ul className='flex flex-row 2xl:justify-end xl:justify-end md:justify-center sm:justify-center justify-center gap-4'>
+        <ul className='flex flex-row 2xl:justify-end xl:justify-end md:justify-center sm:justify-center justify-center gap-2'>
             <li className='2xl:mx-8 xl:mx-6 md:mx-2 sm:mx-2 mt-4'><Link to="inicio" spy={true} smooth={true} offset={-50} duration={1000}>Inicio</Link></li>
             <li className='2xl:mx-8 xl:mx-6 md:mx-2 sm:mx-2 mt-4'><Link to="about" spy={true} smooth={true} offset={-100} duration={1000}>Sobre Mi</Link></li>
             <li className='2xl:mx-8 xl:mx-6 md:mx-2 sm:mx-2 mt-4'><Link to="proyectos" spy={true} smooth={true} offset={0} duration={1000}>Proyectos</Link></li>
--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -20,119 +20,101 @@ import logoAws from '../img/logo-aws.png'
 const Proyectos = () => {
   return (
     <div id='proyectos' className='pt-12 m-auto'>
-    <h2 className='text-3xl font-semibold text-center mt-20 h-28'>Proyectos</h2>
-    <div className='grid grid-cols-2 grid-rows-2 place-content-center justify-center items-center mx-8 grid-proyectos'>
-      
-      
+      <h2 className='text-3xl font-semibold text-center mt-20 h-28'>Proyectos</h2>
+      <div className='grid grid-cols-2 grid-rows-2 place-content-center justify-center items-center mx-8 grid-proyectos'>
         <div>
-        <div className='w-100 mt-10 div-proyectos'>
-        <h3 className='font-semibold text-center'>Workout-Tracker
-          
-        </h3>
-        <a target="_blank" href="https://workout-counter-pa3juduau-francociddiaz.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={workoutTracker} alt="" /></a>
-        </div>
-        <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
-          <img  src={logoReact} alt="" />
-          <img  src={logoBootstrap} alt="" />
-          <img  src={logoAws} alt="" />
-          
-        </div>
-        <div>
-          <a target="_blank" href="https://github.com/FrancoCidDiaz/workout-counter"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000' src={logoGithub} alt="" /></a>
-        </div>
+          <div className='w-100 mt-10 div-proyectos'>
+            <h3 className='font-semibold text-center'>Workout-Tracker
+            </h3>
+            <a target="_blank" href="https://workout-counter-pa3juduau-francociddiaz.vercel.app/"><img className='w-100 rounded-xl h-52 shadow-lg hover:shadow-black duration-1000 mt-6 m-auto' src={workoutTracker} alt="" /></a>
+          </div>
+          <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
+            <img src={logoReact} alt="" />
+            <img src={logoBootstrap} alt="" />
+            <img src={logoAws} alt="" />
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/FrancoCidDiaz/workout-counter"><img className='w-12 mx-auto mt-4 rounded-full shadow-lg hover:shadow-black duration-1000' src={logoGithub} alt="" /></a>
+          </div>
         </div>
         <div>
-        <div className='w-100'>
-        <h3 className='font-semibold text-center'>Blog con login de usuarios
-          
-        </h3>
-        <a target="_blank" href="https://frontend-blog-9a1a.vercel.app/registro">
-        <img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={blog} alt="" /></a>
-        </div>
-        <div className='w-10 h-auto flex justify-center mx-auto gap-2 pt-8'>
-          <img  src={logoReact} alt="" />
-          <img  src={logoReactHook} alt="" />
-          <img  src={logoRouter} alt="" />
-          <img  src={logoNode} alt="" />
-          <img  src={logoMongo} alt="" />
-          <img  src={logoTailwind} alt="" />
-        </div>
-        <div>
-          <a target="_blank" href="https://github.com/FrancoCidDiaz/Frontend-Blog"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000 m-auto' src={logoGithub} alt="" /></a>
-        </div>
-        </div>
-
-        <div>
-        <div className='w-100 mt-10 div-proyectos'>
-        <h3 className='font-semibold text-center'>Quiz Formula 1
-          
-        </h3>
-        <a target="_blank" href="https://quiz-f1.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={quiz} alt="" /></a>
-        </div>
-        <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
-          <img  src={logoReact} alt="" />
-          <img  src={logoBootstrap} alt="" />
-          <img  src={logoRouter} alt="" />
+          <div className='w-100'>
+            <h3 className='font-semibold text-center'>Blog con login de usuarios
+            </h3>
+            <a target="_blank" href="https://frontend-blog-9a1a.vercel.app/registro">
+              <img className='w-100 rounded-xl h-52 shadow-lg hover:shadow-black duration-1000 mt-6 m-auto' src={blog} alt="" /></a>
+          </div>
+          <div className='w-10 h-auto flex justify-center mx-auto gap-2 pt-8'>
+            <img src={logoReact} alt="" />
+            <img src={logoReactHook} alt="" />
+            <img src={logoRouter} alt="" />
+            <img src={logoNode} alt="" />
+            <img src={logoMongo} alt="" />
+            <img src={logoTailwind} alt="" />
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/FrancoCidDiaz/Frontend-Blog"><img className='w-12 mx-auto mt-4 rounded-full shadow-lg hover:shadow-black duration-1000 m-auto' src={logoGithub} alt="" /></a>
+          </div>
         </div>
         <div>
-          <a target="_blank" href="https://github.com/FrancoCidDiaz/quiz-f1"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000' src={logoGithub} alt="" /></a>
-        </div>
-        </div>
-
-       
-        <div>
-        <div className='w-100 mt-10 div-proyectos'>
-        <h3 className='font-semibold text-center'>Tip-Calculator
-          
-        </h3>
-        <a target="_blank" href="https://tip-calculator-seven-rose.vercel.app"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={tipCalculator} alt="" /></a>
-        </div>
-        <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
-          <img  src={logoReact} alt="" />
-          <img  src={logoTailwind} alt="" />
-          
+          <div className='w-100 mt-10 div-proyectos'>
+            <h3 className='font-semibold text-center'>Quiz Formula 1
+            </h3>
+            <a target="_blank" href="https://quiz-f1.vercel.app/"><img className='w-100 rounded-xl h-52 shadow-lg hover:shadow-black duration-1000 mt-6 m-auto' src={quiz} alt="" /></a>
+          </div>
+          <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
+            <img src={logoReact} alt="" />
+            <img src={logoBootstrap} alt="" />
+            <img src={logoRouter} alt="" />
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/FrancoCidDiaz/quiz-f1"><img className='w-12 mx-auto rounded-full mt-4 shadow-lg hover:shadow-black duration-1000' src={logoGithub} alt="" /></a>
+          </div>
         </div>
         <div>
-          <a target="_blank" href="https://github.com/FrancoCidDiaz/tip-calculator"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000' src={logoGithub} alt="" /></a>
-        </div>
-        </div>
-        <div>
-        <div className='w-100 mt-10 div-proyectos'>
-        <h3 className='font-semibold text-center'>Calculadora
-          
-        </h3>
-        <a target="_blank" href="https://calculadora-9yncsoeod-francociddiaz.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={calculadora} alt="" /></a>
-        </div>
-        <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
-          <img  src={logoReact} alt="" />
-          <img  src={logoTailwind} alt="" />
-          <img  src={logoAws} alt="" />
-          
+          <div className='w-100 mt-10 div-proyectos'>
+            <h3 className='font-semibold text-center'>Tip-Calculator
+            </h3>
+            <a target="_blank" href="https://tip-calculator-seven-rose.vercel.app"><img className='w-100 rounded-xl h-52 shadow-lg hover:shadow-black duration-1000 mt-6 m-auto' src={tipCalculator} alt="" /></a>
+          </div>
+          <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
+            <img src={logoReact} alt="" />
+            <img src={logoTailwind} alt="" />
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/FrancoCidDiaz/tip-calculator"><img className='w-12 mx-auto rounded-full mt-4 shadow-lg hover:shadow-black duration-1000' src={logoGithub} alt="" /></a>
+          </div>
         </div>
         <div>
-          <a target="_blank" href="https://github.com/FrancoCidDiaz/calculadora"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000' src={logoGithub} alt="" /></a>
-        </div>
+          <div className='w-100 mt-10 div-proyectos'>
+            <h3 className='font-semibold text-center'>Calculadora
+            </h3>
+            <a target="_blank" href="https://calculadora-9yncsoeod-francociddiaz.vercel.app/"><img className='w-100 rounded-xl h-52 shadow-lg hover:shadow-black duration-1000 mt-6 m-auto' src={calculadora} alt="" /></a>
+          </div>
+          <div className='w-10  flex justify-center mx-auto gap-4 pt-8'>
+            <img src={logoReact} alt="" />
+            <img src={logoTailwind} alt="" />
+            <img src={logoAws} alt="" />
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/FrancoCidDiaz/calculadora"><img className='w-12 shadow-lg rounded-full hover:shadow-black mx-auto mt-4 duration-1000' src={logoGithub} alt="" /></a>
+          </div>
         </div>
         <div>
-        <div className='w-100 '>
-        <h3 className='font-semibold text-center'>Academia virtual
-          
-        </h3>
-        <a target="_blank" href="https://integrador-nucba-dise-o-web.vercel.app/"><img className='w-100 rounded-xl h-52 hover:h-60 duration-1000 mt-6 m-auto' src={academia} alt="" /></a>
+          <div className='w-100'>
+            <h3 className='font-semibold text-center'>Academia virtual
+            </h3>
+            <a target="_blank" href="https://integrador-nucba-dise-o-web.vercel.app/"><img className='w-100 rounded-xl h-52 shadow-lg hover:shadow-black duration-1000 mt-6 m-auto' src={academia} alt="" /></a>
+          </div>
+          <div className='w-12 flex justify-center mx-auto gap-4 pt-8'>
+            <img src={logoHtml} alt="" />
+            <img src={logoCss} alt="" />
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/FrancoCidDiaz/Integrador-nucba-dise-o-web"><img className='w-12 rounded-full mx-auto mt-4 shadow-lg hover:shadow-black duration-1000 m-auto' src={logoGithub} alt="" /></a>
+          </div>
         </div>
-        <div className='w-12 flex justify-center mx-auto gap-4 pt-8'>
-          <img  src={logoHtml} alt="" />
-          <img  src={logoCss} alt="" />
-        </div>
-        <div>
-          <a target="_blank" href="https://github.com/FrancoCidDiaz/Integrador-nucba-dise-o-web"><img className='w-12 mx-auto mt-4 hover:w-16 duration-1000 m-auto' src={logoGithub} alt="" /></a>
-        </div>
-        </div>
-
-
-
-     
-    </div>
+      </div>
     </div>
   )
 }
